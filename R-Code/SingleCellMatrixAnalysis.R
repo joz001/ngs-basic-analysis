0d21b402ab557630f2c18169b081fdf020a2a061
Jonathan Zhu <joz001@ucsd.edu>
2023-10-29 15:29:31 -0700

    Update scrna seq analysis

--- R-Code/SingleCellMatrixAnalysis.R
+++ R-Code/SingleCellMatrixAnalysis.R
@@ -1,51 +1,58 @@
-library(SingleCellExperiment)
-library(tictoc)
 library(dplyr)
+library(data.table)
 library(ggplot2)
-setwd('/data/scrna_seq/count_matrices/')
-files = list.files('/data/scrna_seq/count_matrices/')
 
-make_counts <- function(x){
-  f1 = read.table(x, header=TRUE, row.names=1)
-  colnames(f1) = paste(substring(x, 1, 10), colnames(f1), sep='_')
-  return(f1)
+
+read_in <- function(file){
+  
+  f = read.csv(file, sep='\t', row.names=1)
+  f = f[colSums(f) >= 6000]
+  f = f[rowSums(f) > 0,]
+  f = setDT(f, keep.rownames = TRUE)
+  colnames(f) = c('genes', paste(colnames(f)[-1], "_", strsplit(file, '[.]')[[1]][1], sep=""))
+  return(f)
 }
 
-merge_by_rownames <- function(x, y){
-  merged = merge(x, y, by=0, all=TRUE) %>%
-    transform(row.names = Row.names, Row.names=NULL)
-  merged[is.na(merged)] = 0
-  return(merged)
+merge_files <- function(file_1, file_2){
+  return(merge(file_1, file_2, all=TRUE))
 }
+
 
 {
-  tic()
-  counts = lapply(files, make_counts)
-  toc()
-  tic()
-  all_counts = Reduce(merge_by_rownames, counts) 
-  toc()
+  start = Sys.time()
+  files = list.files() %>% unlist %>%
+    lapply(read_in)
+  count_mat = Reduce(merge_files, files)
+  count_mat[is.na(count_mat)] = 0
+  print(Sys.time() - start)
 }
 
-total_counts = colSums(all_counts) %>% data.frame('sample_counts' = .)
-filter_6k_transcripts = total_counts %>% filter(sample_counts >= 6000)
-min_6k = all_counts[colnames(all_counts) %in% row.names(filter_6k_transcripts)]
+norm_count_mat = count_mat[,-1]
+norm_count_mat = mapply("/", norm_count_mat, colSums(norm_count_mat))
+norm_count_mat = norm_count_mat * 6000
+#row.names(norm_count_mat) = count_mat[,1][[1]]
 
-min_6k$rowsums = rowSums(min_6k)
-min_6k = min_6k[min_6k$rowsums > 0, ]
+pre_pca = t(norm_count_mat)
+pre_pca = pre_pca[, which(apply(pre_pca, 2, var) != 0)]
+pca <- prcomp(pre_pca, scale=TRUE)
+pca.data <- data.frame(Sample = rownames(pca$x),
+                       X = pca$x[,1], Y = pca$x[,2])
+min_pca_axis = min(pca.data$X, pca.data$Y)
+max_pca_axis = max(pca.data$X, pca.data$Y)
+pca.var <- pca$sdev^2
+pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
 
-colsums = colSums(min_6k)
-norm = mapply('/', min_6k, colsums)
+p <- pca.data %>% 
+  ggplot(aes(x=X, y=Y, label=Sample)) + 
+  geom_point(size=3) +
+  labs(title = 'title',
+       x = paste("PC1 - ", pca.var.per[1], "%", sep = ""), 
+       y = paste("PC2 - ", pca.var.per[2], "%", sep = "")) +
+  theme_bw() + 
+  scale_x_continuous(limits = c(min_pca_axis * 1.25, max_pca_axis * 1.25)) +
+  scale_y_continuous(limits = c(min_pca_axis * 1.25, max_pca_axis * 1.25)) +
+  theme(legend.title = element_blank(),
+        legend.position = 'top',
+        axis.title = element_text())
 
-ggplot(data = total_counts, aes(x=log10(sample_counts))) +
-  geom_bar(stat='bin', binwidth=.5)
-   
-
-freq = ggplot(all_counts, aes(x=rowSums)) +
-  geom_histogram(binwidth = 1) +
-  xlim(0, 10000)
-  #ylim(0, 200)
-freq
-
-p = pca_plot(all_counts[1:3019], '', '')
 p
